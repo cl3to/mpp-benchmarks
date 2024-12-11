23606722682d52121c1f3a5eb05df55da213e38b
Jhonatan Cléto <j256444@dac.unicamp.br>
2024-12-11 07:57:41 -0300

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Build Microbenchmark (MPPBench)
+cd ./microbenchmark
+CUDA_ARCH=$1 make
+cp -f ./MPPBench ../
+make clean
+
+# Build RSBench
+cd ../proxy_apps/RSBench
+CUDA_ARCH=$1 make
+cp -f ./RSBench-* ../../
+make clean
+
+# Build XSBench
+cd ../XSBench
+CUDA_ARCH=$1 make
+cp -f ./XSBench-* ../../
+make clean
+
+cd ../../
